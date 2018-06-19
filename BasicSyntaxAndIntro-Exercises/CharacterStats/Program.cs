fd890052d46b99b33b984467791768fb17c2519d
Nelly Nikolova <32382624+NeliNikolova@users.noreply.github.com>
2018-06-19 14:37:58 +0300

    Update Program.cs

--- BasicSyntaxAndIntro-Exercises/CharacterStats/Program.cs
+++ BasicSyntaxAndIntro-Exercises/CharacterStats/Program.cs
@@ -1,15 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CharacterStats
+namespace CaracterStat
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			string name = Console.ReadLine();
+			int currentHealth = int.Parse(Console.ReadLine());
+			int maximumHealth = int.Parse(Console.ReadLine());
+			int currentEnargy = int.Parse(Console.ReadLine());
+			int maximumEnergy = int.Parse(Console.ReadLine());
+			int resultHealth = maximumHealth - currentHealth;
+			int resultEnergy = maximumEnergy - currentEnargy;
+
+
+			Console.WriteLine("Name: {0}", name);
+			Console.WriteLine("Health: |{0}{1}|", new string('|', currentHealth), new string('.', resultHealth));
+			Console.WriteLine("Energy: |{0}{1}|", new string('|', currentEnargy), new string('.', resultEnergy));
+
+
 		}
 	}
 }
+
