70e1a16442ed051a02f048746ef59e51d919deae
Nelly Nikolova <32382624+NeliNikolova@users.noreply.github.com>
2018-06-19 14:38:34 +0300

    Update Program.cs

--- BasicSyntaxAndIntro-Exercises/BeverageLabel/Program.cs
+++ BasicSyntaxAndIntro-Exercises/BeverageLabel/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,15 @@ namespace BeverageLabel
 	{
 		static void Main(string[] args)
 		{
+			string name = Console.ReadLine();
+			double volume = int.Parse(Console.ReadLine());
+			double energy = int.Parse(Console.ReadLine());
+			double sugar = int.Parse(Console.ReadLine());
+			double kiloCal = (volume * energy) / 100;
+			double sugarTotal = (volume / 100) * sugar;
+			Console.WriteLine("{0}ml {1}:", volume, name);
+			Console.WriteLine("{0}kcal, {1}g sugars", kiloCal, sugarTotal);
 		}
 	}
 }
+
