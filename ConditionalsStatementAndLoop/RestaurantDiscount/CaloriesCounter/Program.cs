23a4fb96e0a8b5552304d0160250a0fdf2da0aba
Nelly Nikolova <32382624+NeliNikolova@users.noreply.github.com>
2018-06-19 17:21:55 +0300

    Add files via upload

---
+++ ConditionalsStatementAndLoop/RestaurantDiscount/CaloriesCounter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloriesCounter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int numberOfLines = int.Parse(Console.ReadLine());
+			int sumOfCalories = 0;
+			for (int i = 0; i < numberOfLines; i++)
+			{
+				string ingredients = Console.ReadLine();
+				ingredients = ingredients.ToLower();
+
+				if (ingredients == "cheese")
+				{
+					sumOfCalories += 500;
+
+				}
+				else if (ingredients == "tomato sauce")
+				{
+					sumOfCalories += 150;
+				}
+				else if (ingredients == "salami")
+				{
+					sumOfCalories += 600;
+				}
+				else if (ingredients == "pepper")
+				{
+					sumOfCalories += 50;
+				}
+
+
+			}
+			Console.WriteLine($"Total calories: {sumOfCalories}");
+		}
+	}
