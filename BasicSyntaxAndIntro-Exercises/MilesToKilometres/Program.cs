83043ad22ac5d3dba2aabe2bab20224a389e3f4a
Nelly Nikolova <32382624+NeliNikolova@users.noreply.github.com>
2018-06-19 14:33:21 +0300

    Update Program.cs

--- BasicSyntaxAndIntro-Exercises/MilesToKilometres/Program.cs
+++ BasicSyntaxAndIntro-Exercises/MilesToKilometres/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MilesToKilometres
+namespace mile
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			double mile = double.Parse(Console.ReadLine());
+			double kilometer = mile * 1.60934;
+			Console.WriteLine("{0:f2}", kilometer);
 		}
 	}
 }
