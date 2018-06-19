39ab616c6df19d5a061b6ad799c3a8f6a5d6af17
Nelly Nikolova <32382624+NeliNikolova@users.noreply.github.com>
2018-06-19 15:09:28 +0300

    Add Conditional Statements and Loops files

---
+++ ConditionalsStatementAndLoop/ChooseADrink/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proffesion
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string proffesion = Console.ReadLine();
+			if (proffesion == "Athlete")
+			{
+				Console.WriteLine("Water");
+			}
+			else if (proffesion == "Businessman" || proffesion == "Businesswoman")
+			{
+				Console.WriteLine("Coffee");
+			}
+			else if (proffesion == "SoftUni Student")
+			{
+				Console.WriteLine("Beer");
+			}
+			else
+			{
+				Console.WriteLine("Tea");
+			}
+		}
+	}
+}
---
+++ ConditionalsStatementAndLoop/ChooseADrink2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Choose_a_drink
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string proffesion = Console.ReadLine();
+			int quantity = int.Parse(Console.ReadLine());
+			double price = 0;
+			if (proffesion == "Athlete")
+			{
+				price = 0.7 * quantity;
+
+
+			}
+			else if (proffesion == "Businessman" || proffesion == "Businesswoman")
+			{
+				price = 1.0 * quantity;
+
+			}
+			else if (proffesion == "SoftUni Student")
+			{
+				price = 1.7 * quantity;
+
+			}
+			else
+			{
+				price = 1.2 * quantity;
+
+			}
+			Console.WriteLine($"The { proffesion} has to pay {price:f2}.");
+		}
+	}
+}
